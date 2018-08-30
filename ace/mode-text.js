c0f13e1c41c7ccc52073d266cc012ad763ac4ab0
spck-io <spckorg@gmail.com>
2018-08-30 00:30:18 -0400

    Updates

--- ace/mode-text.js
+++ ace/mode-text.js
@@ -1,4 +1,5 @@
-;                (function() {
+;
+                (function() {
                     window.require(["ace/mode/text"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
