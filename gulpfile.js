bad13eb591319eed70d8fbf25bd4150f66ca91c7
spck-io <spckorg@gmail.com>
2020-03-20 22:15:27 -0400

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var umd = require('gulp-umd');
-var pump = require('pump');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const umd = require('gulp-umd');
+const pump = require('pump');
 
-gulp.task('build', ['build-minify', 'build-debug']);
-
-gulp.task('build-debug', function () {
+function buildDebug () {
   return gulp.src(['src/**/*.js'])
     .pipe(concat('spck-embed.js'))
     .pipe(umd({
@@ -18,13 +16,21 @@ gulp.task('build-debug', function () {
       }
     }))
     .pipe(gulp.dest('./dist'));
-});
+}
 
-gulp.task('build-minify', ['build-debug'], function (cb) {
+function buildProd (cb) {
   pump([
     gulp.src(['dist/spck-embed.js']),
     concat('spck-embed.min.js'),
     uglify(),
     gulp.dest('./dist/')
   ], cb);
-});
+}
+
+const build = gulp.parallel(buildDebug, buildProd)
+
+module.exports = {
+  buildDebug,
+  buildProd,
+  build
+}
